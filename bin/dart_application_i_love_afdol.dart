568d4f426826403217ce3b2e0db86c1d5ed3c82d
mamipoko22 <213728790+mamipoko22@users.noreply.github.com>
2025-11-05 13:18:49 +0700

    first

---
+++ bin/dart_application_i_love_afdol.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_i_love_afdol/dart_application_i_love_afdol.dart' as dart_application_i_love_afdol;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_i_love_afdol.calculate()}!');
+}
